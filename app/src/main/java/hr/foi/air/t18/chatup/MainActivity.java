ccd3fe900158a726f143dad0e05ee1d6ce36b6c9
JurmanLap <mjurman@gmail.com>
2015-10-21 20:56:08 +0200

    - Added new activity class (RegisterActivity.java) - Defined new activity layout (activity_register.xml) - Overrided 0nCreate handler and called setContentView - Registered new activity in AndroidManifest.xml - Added button "Register" in activity_main.xml - Added all necessary strings, dimens and colors (colors.xml, dimens.xml, strings.xml) - CREATED REGISTRATION FORM (activity_register.xml)

--- app/src/main/java/hr/foi/air/t18/chatup/MainActivity.java
+++ app/src/main/java/hr/foi/air/t18/chatup/MainActivity.java
@@ -1,7 +1,11 @@
 package hr.foi.air.t18.chatup;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find reference to btn_register and subscribe to onClick event.
+        Button btnRegister = (Button)findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+            startActivity(i);
+            }
+        });
     }
 }
