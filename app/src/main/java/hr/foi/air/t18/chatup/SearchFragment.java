5857416f9ffd7086b937a517d26e0407836318ad
mjurman <jurman.matija@gmail.com>
2015-11-21 14:11:36 +0100

    Dio logike za pretrazivanje korisnika: - Kreirana klasa SearchAsync koja će komunicirati sa  bazom podataka preko web servisa u svrhu pretrage registriranih korisnika (SearchAsync.java) - Malo editirana klasa SearchFragment, spremna za implementaciju logike prilikom klika na gumb 'Search' (SearchFragment.java)

--- app/src/main/java/hr/foi/air/t18/chatup/SearchFragment.java
+++ app/src/main/java/hr/foi/air/t18/chatup/SearchFragment.java
@@ -1,10 +1,14 @@
 package hr.foi.air.t18.chatup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Simple Fragment for searching other users
@@ -14,6 +18,19 @@ public class SearchFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_search, container, false);
+
+        View search_view = inflater.inflate(R.layout.tab_fragment_search, container, false);
+        Button search_button = (Button) search_view.findViewById(R.id.searchButton);
+        search_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //search logic here
+                Toast.makeText(getContext(),"You clicked search" , Toast.LENGTH_LONG).show();
+            }
+        });
+        return search_view;
+        // return inflater.inflate(R.layout.tab_fragment_search, container, false);
     }
 }
---
+++ webservice/src/main/java/hr/foi/air/t18/webservice/SearchAsync.java
@@ -0,0 +1,73 @@
+package hr.foi.air.t18.webservice;
+
+import android.os.AsyncTask;
+import org.json.JSONObject;
+import java.util.HashMap;
+import hr.foi.air.t18.core.HttpPOST;
+
+/**
+ * This class is used to communicate with database through web service. The class purpose is to
+ * get e-mail and username of registered users in database.
+ * The class creates HTTP request with e-mail and username  as parameters, it sends the request to
+ * the web service and waits for its response.
+ * Created by JurmanLap on 19.11.2015..
+ */
+public class SearchAsync extends AsyncTask<Void, Void, String> {
+
+    private String email;
+    private String username;
+    private IListener listener;
+
+    public SearchAsync(String email, String username, IListener listener)
+    {
+        this.username = username;
+        this.email = email;
+        this.listener = listener;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        this.listener.onBegin();
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        String response;
+
+        HashMap<String, String> parameters = new HashMap<>();
+        parameters.put("mail",this.email);
+        parameters.put("username",this.username);
+
+        try
+        {
+            HttpPOST connection = new HttpPOST("http://104.236.58.50:8080/search");
+            connection.sendRequest(parameters);
+            response = connection.getResponse();
+        }
+        catch (Exception e)
+        {
+            response = e.getMessage();
+        }
+
+        return response;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        String message;
+        int status;
+        try
+        {
+            JSONObject json = new JSONObject(result);
+            message = json.getString("message");
+            status = json.getInt("status");
+        }
+        catch (Exception e)
+        {
+            message = "Error parsing JSON. Either JSON is invalid or server is down.";
+            status = -1;
+        }
+
+        this.listener.onFinish(status, message);
+    }
+}
