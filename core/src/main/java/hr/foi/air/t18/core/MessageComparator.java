454968077a8d6b93dff34fcaf3024b54fb587ebd
Danijel Filipovic <dfilipov@foi.hr>
2015-12-06 14:49:21 +0100

    > Omogućeno je slanje poruka > !! Zasad svaki pošiljatelj poruke je Mirko !!

---
+++ core/src/main/java/hr/foi/air/t18/core/MessageComparator.java
@@ -0,0 +1,7 @@
+package hr.foi.air.t18.core;
+
+/**
+ * Created by Danijel on 5.12.2015..
+ */
+public class MessageComparator {
+}
---
+++ webservice/src/main/java/hr/foi/air/t18/webservice/SendMessageAsync.java
@@ -0,0 +1,7 @@
+package hr.foi.air.t18.webservice;
+
+/**
+ * Created by Danijel on 6.12.2015..
+ */
+public class SendMessageAsync {
+}
