a641a076417ebfac6b0868328be033de6de5ccbf
mjurman <jurman.matija@gmail.com>
2015-12-15 23:09:08 +0100

    Funkcionalnost: Search tab prikazuje listu svih registriranih korisnika --potrebno jos napraviti: filtriranje korisnika i dodavanje korisnika u listu prijatelja
    -Azurirana logika prikaza registriranih korisnika (SearchFragment.java)
    -Azurirana klasa SearchAsync.java (komunikacija sa web servisom)

--- app/src/main/java/hr/foi/air/t18/chatup/SearchFragment.java
+++ app/src/main/java/hr/foi/air/t18/chatup/SearchFragment.java
@@ -1,36 +1,67 @@
 package hr.foi.air.t18.chatup;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ListView;
 import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import hr.foi.air.t18.core.User;
+import hr.foi.air.t18.webservice.IListener;
+import hr.foi.air.t18.webservice.SearchAsync;
+import hr.foi.air.t18.webservice.WebServiceResult;
 
 /**
- * Simple Fragment for searching other users
  * Created by Laptop on 9.11.2015..
  */
 public class SearchFragment extends Fragment {
 
+    private SharedPreferences sharedPref;
+    private String loggedIn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //return inflater.inflate(R.layout.tab_fragment_main, container, false);
+        setRetainInstance(true);
+        this.sharedPref = getActivity().getPreferences(getActivity().MODE_PRIVATE);
+        this.loggedIn = sharedPref.getString("id","unknown");
 
-        View search_view = inflater.inflate(R.layout.tab_fragment_search, container, false);
-        Button search_button = (Button) search_view.findViewById(R.id.searchButton);
-        search_button.setOnClickListener(new View.OnClickListener()
-        {
+        View root = inflater.inflate(R.layout.tab_fragment_search, container, false);
+        final ArrayList<User> users = new ArrayList<User>();
+
+        final View final_root = root;
+        SearchAsync registeredUsers = new SearchAsync(this.loggedIn, new IListener<JSONArray>() {
             @Override
-            public void onClick(View v)
-            {
-                //search logic here
-                Toast.makeText(getContext(),"You clicked search" , Toast.LENGTH_LONG).show();
+            public void onBegin() {
+            }
+
+            @Override
+            public void onFinish(WebServiceResult<JSONArray> result) {
+                if (result.status == 0) {
+                    try {
+                        JSONArray json = result.data;
+                        for (int i = 0; i < json.length(); i++) {
+                            JSONObject currentUser = json.getJSONObject(i);
+                            users.add(new User(currentUser.getString("id"), currentUser.getString("id"), currentUser.getString("status")));
+                        }
+                        ListView lv = (ListView) final_root.findViewById(R.id.searchListview);
+                        lv.setAdapter(new UserListAdapter(getActivity(), users));
+                    } catch (Exception e) {
+                        Toast.makeText(getActivity().getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(getActivity().getApplicationContext(), result.message, Toast.LENGTH_LONG).show();
+                }
             }
         });
-        return search_view;
-        // return inflater.inflate(R.layout.tab_fragment_search, container, false);
+
+        registeredUsers.execute();
+
+        return root;
     }
 }
