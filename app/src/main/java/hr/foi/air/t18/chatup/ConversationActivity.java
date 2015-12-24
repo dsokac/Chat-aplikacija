2dffa5efd22226821a86a80b29c613845bc5ed6d
Danijel Filipovic <dfilipov@foi.hr>
2015-12-24 12:37:06 +0100

    Omogućeno osvježavanje razgovora, tj. dohvaćanje novih poruka unutar aktivnosti za razgovore

--- app/src/main/java/hr/foi/air/t18/chatup/ConversationActivity.java
+++ app/src/main/java/hr/foi/air/t18/chatup/ConversationActivity.java
@@ -1,6 +1,7 @@
 package hr.foi.air.t18.chatup;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -9,7 +10,10 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import hr.foi.air.t18.core.Conversation;
 import hr.foi.air.t18.core.MessageComparator;
@@ -17,6 +21,7 @@ import hr.foi.air.t18.core.Message;
 import hr.foi.air.t18.core.MiddleMan;
 import hr.foi.air.t18.core.SharedPreferencesClass;
 import hr.foi.air.t18.webservice.IListener;
+import hr.foi.air.t18.webservice.RefreshConversationAsync;
 import hr.foi.air.t18.webservice.SendMessageAsync;
 import hr.foi.air.t18.webservice.WebServiceResult;
 
@@ -27,6 +32,8 @@ public class ConversationActivity extends AppCompatActivity
     private Button btnSendMessage;
     private EditText txtMessage;
 
+    private Timer refreshTimer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -36,22 +43,46 @@ public class ConversationActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        conversation = (Conversation) MiddleMan.getObject();
-        lvMessages = (ListView) findViewById(R.id.convMessages);
-        btnSendMessage = (Button) findViewById(R.id.convSendButton);
-        txtMessage = (EditText) findViewById(R.id.convTextBox);
-
+        InitializeComponents();
         AddEvents();
+
         SortAndLoadMessagesIntoListView();
         lvMessages.setSelection(lvMessages.getCount() - 1);
     }
 
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        refreshTimer.cancel();
+        refreshTimer.purge();
+    }
+
+    private void InitializeComponents()
+    {
+        conversation = (Conversation) MiddleMan.getObject();
+        lvMessages = (ListView) findViewById(R.id.convMessages);
+        btnSendMessage = (Button) findViewById(R.id.convSendButton);
+        txtMessage = (EditText) findViewById(R.id.convTextBox);
+    }
+
     private void AddEvents()
+    {
+        AddSendMessageEvent();
+        AddRefreshEvent();
+    }
+
+    private void SortAndLoadMessagesIntoListView()
+    {
+        Collections.sort(conversation.getMessages(), new MessageComparator());
+        lvMessages.setAdapter(new MessagesListAdapter(conversation.getMessages(), this));
+    }
+
+    private void AddSendMessageEvent()
     {
         btnSendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 final View view = v;
 
                 Message message = new Message(
@@ -63,19 +94,17 @@ public class ConversationActivity extends AppCompatActivity
                 );
                 SendMessageAsync sm = new SendMessageAsync(conversation, message, new IListener<Message>() {
                     @Override
-                    public void onBegin() {}
+                    public void onBegin() {
+                    }
 
                     @Override
-                    public void onFinish(WebServiceResult<Message> result)
-                    {
-                        if (result.status == 0)
-                        {
+                    public void onFinish(WebServiceResult<Message> result) {
+                        if (result.status == 0) {
                             txtMessage.setText("");
                             conversation.addMessage(result.data);
                             SortAndLoadMessagesIntoListView();
                             lvMessages.setSelection(lvMessages.getCount() - 1);
-                        }
-                        else
+                        } else
                             Toast.makeText(view.getContext(), result.message, Toast.LENGTH_LONG).show();
                     }
                 });
@@ -84,9 +113,54 @@ public class ConversationActivity extends AppCompatActivity
         });
     }
 
-    private void SortAndLoadMessagesIntoListView()
+    private void AddRefreshEvent()
     {
-        Collections.sort(conversation.getMessages(), new MessageComparator());
-        lvMessages.setAdapter(new MessagesListAdapter(conversation.getMessages(), this));
+        refreshTimer = new Timer();
+        final Handler refreshHandler = new Handler();
+
+        TimerTask refreshEvent = new TimerTask()
+        {
+            @Override
+            public void run() {
+                Runnable refreshThread = new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        CallRefreshConversationAsyncTask();
+                    }
+                };
+                refreshHandler.post(refreshThread);
+            }
+        };
+
+        refreshTimer.schedule(refreshEvent, 1000, 5000);
+    }
+
+    private void CallRefreshConversationAsyncTask()
+    {
+        int numberOfMessages = conversation.getMessages().size();
+        String lastTimestamp = conversation.getMessages().get(numberOfMessages - 1).getTimeSend();
+        RefreshConversationAsync rc = new RefreshConversationAsync(
+                conversation.getID(),
+                lastTimestamp,
+                new IListener<ArrayList<Message>>()
+                {
+                    @Override
+                    public void onBegin() {}
+
+                    @Override
+                    public void onFinish(WebServiceResult<ArrayList<Message>> result)
+                    {
+                        if (result.status == 0 && result.data.size() > 0)
+                        {
+                            for (int i = 0; i < result.data.size(); i++)
+                                conversation.addMessage(result.data.get(i));
+                            SortAndLoadMessagesIntoListView();
+                        }
+                    }
+                }
+        );
+        rc.execute();
     }
 }
---
+++ webservice/src/main/java/hr/foi/air/t18/webservice/RefreshConversationAsync.java
@@ -0,0 +1,105 @@
+package hr.foi.air.t18.webservice;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import hr.foi.air.t18.core.HttpPOST;
+import hr.foi.air.t18.core.Message;
+
+/**
+ * Created by Danijel on 24.12.2015..
+ */
+public class RefreshConversationAsync extends AsyncTask<Void, Void, String>
+{
+    String chatID;
+    String timestamp;
+    IListener<ArrayList<Message>> listener;
+
+    public RefreshConversationAsync(String chatID, String timestamp, IListener<ArrayList<Message>> listener)
+    {
+        this.chatID = chatID;
+        this.timestamp = timestamp;
+        this.listener = listener;
+    }
+
+    @Override
+    protected String doInBackground(Void... params)
+    {
+        String response;
+
+        HashMap<String, String> parameters = new HashMap<>();
+        parameters.put("chatID", chatID);
+        parameters.put("timestamp", timestamp);
+
+        try
+        {
+            HttpPOST connection = new HttpPOST("http://104.236.58.50:8080/getNewMessages");
+            connection.sendRequest(parameters);
+            response = connection.getResponse();
+        }
+        catch (Exception e)
+        {
+            response = e.getMessage();
+        }
+
+        return response;
+    }
+
+    @Override
+    protected void onPostExecute(String result)
+    {
+        int status;
+        String message;
+        ArrayList<Message> data;
+
+        try
+        {
+            JSONObject jsonObject = new JSONObject(result);
+            status = jsonObject.getInt("status");
+            message = jsonObject.getString("message");
+            data = status == 0
+                    ? GetNewMessages(jsonObject.getString("data"))
+                    : null;
+        }
+        catch (Exception e)
+        {
+            status = -1;
+            message = e.getMessage();
+            data = null;
+        }
+
+        WebServiceResult<ArrayList<Message>> wsResult = new WebServiceResult<>();
+        wsResult.status = status;
+        wsResult.message = message;
+        wsResult.data = data;
+
+        listener.onFinish(wsResult);
+    }
+
+    private ArrayList<Message> GetNewMessages(String data) throws JSONException
+    {
+        ArrayList<Message> messages = new ArrayList<>();
+        JSONArray jsonArray = new JSONArray(data);
+
+        for (int i = 0; i < jsonArray.length(); i++)
+        {
+            JSONObject jsonObject = new JSONObject(jsonArray.getString(i));
+            Message message = new Message(
+                    jsonObject.getString("text"),
+                    jsonObject.getString("sender"),
+                    jsonObject.getString("timeSend"),
+                    jsonObject.getString("location"),
+                    jsonObject.getString("type")
+            );
+            messages.add(message);
+        }
+
+        return messages;
+    }
+}
