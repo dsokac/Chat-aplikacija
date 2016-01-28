c34cbe595a67c0442c696bfeafb04dec7fbb06d0
dsokac <dsokac@foi.hr>
2016-01-28 19:04:06 +0100

    Pokrenut servis i maknuto smeće iz Android manifesta

--- socketnotifications/src/main/java/hr/foi/air/t18/socketnotifications/BackgroundService.java
+++ socketnotifications/src/main/java/hr/foi/air/t18/socketnotifications/BackgroundService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 /**
  * Class for handling background service.
